d6fcff3622325dca0d71d2e8d36fe63ff6653b8e
Akil Griffiths <akilgg@gmail.com>
2020-03-20 14:23:03 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-VAR cell = ["   ", "   ", "   "]
+ cell = ["   ", "   ", "   "]
 cell.*("|")
   puts [cell, "-----------", cell, "-----------", cell]
 
